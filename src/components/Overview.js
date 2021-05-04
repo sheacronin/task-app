7e612125195849a368bcf113c9676dc04e55b8af
Shea Cronin <shealcronin@gmail.com>
2021-05-04 15:17:10 -0400

    Add edit button to each task

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import trashIcon from '../i/trash.svg';
+import editIcon from '../i/edit.svg';
 
 const Overview = (props) => {
     const { tasks, removeTask } = props;
@@ -11,6 +12,13 @@ const Overview = (props) => {
                     <li key={task.id}>
                         <span className="task-num">{i + 1}</span>
                         {task.text}
+                        <button className="edit-task">
+                            <img
+                                src={editIcon}
+                                alt="pencil edit icon"
+                                onClick={() => console.log('hi')}
+                            />
+                        </button>
                         <button className="delete-task">
                             <img
                                 src={trashIcon}
